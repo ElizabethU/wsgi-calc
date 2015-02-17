acf0c8457f7459c289163b4bed5d35a9151963d4
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2015-02-16 20:46:52 -0800

    added info to main page

--- calculator.py
+++ calculator.py
@@ -2,13 +2,23 @@ import re
 from decimal import *
 
 def home():
-    #all_books = DB.titles()
-    #body = ['<h1>My Bookshelf</h1>', '<ul>']
-    #item_template = '<li><a href="/book/{id}">{title}</a></li>'
-    #for book in all_books:
-    #    body.append(item_template.format(**book))
-    #body.append('</ul>')
-    return '''Welcome to a trivial calculator app!''' #'\n'.join(body)
+    return '''<p>This is my calculator app!</p>
+
+<p>To use, please make a clone of this repo, then in the same directory as calculator.py, type </p>
+
+<p>$ python calculator.py</p>
+
+<p>to run the server, then open http://localhost:8000/ in your browser</p>
+
+<p>This calculator has 4 actions: add, subtract, multiply, & divide.
+To use the calculator, add a / followed by one of those actions, followed by another slash, and then two numbers, separated by a slash.</p>
+
+<p>Example:
+http://localhost:8000/add/8/7 will add the numbers 8 and 7</p>
+
+<p>For subtraction and division, where order of the numbers matters, the first number is the number being subtracted against, or divided by the other number, the order you would expect to find it written out.</p>
+
+<p>It should be noted that this calculator uses decimals, not floats, and is therefore better than the latest OSX calculator.</p>'''
 
 def multiply(num1, num2):
     answer = Decimal(num1) * Decimal(num2)
